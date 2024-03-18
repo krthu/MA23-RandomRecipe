311848eb90320fd34351e76bd212e04a85a25de8
KT <thun.kristian@gmail.com>
2024-03-18 09:46:46 +0100

    Initial Commit

---
+++ MA23_FoodApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MA23_FoodApp
+//
+//  Created by Kristian Thun on 2024-03-18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
